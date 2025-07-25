52b60676b247b5491f6ba9a5f9bb7267142b8f9b
clinton200 <oumaclinton004@gmail.com>
2025-07-26 00:30:59 +0300

    my first PLP_academy assignment

---
+++ my_first_assignment.py
@@ -0,0 +1,9 @@
+# Getting the numbers from the user
+a = int(input("Enter your first number: "))
+b = int(input("Enter the second num ber: "))
+
+print(a+b)  # performing addition
+print(a-b)  # perfoming substration
+print(a*b)  # performing multiplication
+print(a/b)  # performing division
+print(a**b)  # performing power 
